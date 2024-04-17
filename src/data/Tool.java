228a4091adaca2ef359f8865f43fd5094bfffca0
mgodfreyyoung <mgodfreyyoung@gmail.com>
2024-04-17 16:28:38 -0600

    changed the vars inside of Tool data class to be final since I do not think these values will every change onces the tool instance is created.

--- src/data/Tool.java
+++ src/data/Tool.java
@@ -2,9 +2,18 @@ package data;
 
 
 public class Tool {
-    public Charge charge;
 
-    public String toolCodee;
-    public String toolType;
-    public String brande;
+    public Tool(String toolCode, String toolType, String brand, Charge charge){
+        this.toolCode = toolCode;
+        this.toolType = toolType;
+        this.brand = brand;
+
+        this.charge = charge;
+    };
+
+    public final Charge charge;
+
+    public final String toolCode;
+    public final String toolType;
+    public final String brand;
 }
