f0124b704e10344087e561f26288cc9d7bb27297
mgodfreyyoung <mgodfreyyoung@gmail.com>
2024-04-17 19:39:53 -0600

    added junit dependencies for testing; added first test case that test that discount percentage is in range.

---
+++ tests/CheckOutTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class CheckOutTest {
+
+
+    @Test
+    public void testOne() {
+        try {
+            new CheckOut("JAKR", 5, 101f, new Date());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
