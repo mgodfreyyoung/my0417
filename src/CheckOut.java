a6ba9c1de199da1564ca68106e091b39684e37e5
mgodfreyyoung <mgodfreyyoung@gmail.com>
2024-04-17 18:19:11 -0600

    added in the validateRentalDayCount which makes sure that the rental day count is at least one or greater, Currently just throwing this back up to the caller in the constructor and letting it print the error message out to console.

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -2,7 +2,20 @@ import java.util.Date;
 
 public class CheckOut {
 
-    public CheckOut(String toolCode, int rentalDayCount, float discountPercentage, Date checkOutDate){
-
+    public CheckOut(String toolCode, int rentalDayCount, float discountPercentage, Date checkOutDate) {
+        try {
+            validateRentalDayCount(rentalDayCount);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    // validate that the rental day count is at least one or greater
+    // if not throw back up to calling method so it can message user properly.
+    public void validateRentalDayCount(int rentalDayCount) throws Exception {
+        if (rentalDayCount < 1) {
+            throw new Exception("Rental day count is not 1 or greater. Please enter a value for number of rental days that is at least one.");
+        }
+    }
+
 }
