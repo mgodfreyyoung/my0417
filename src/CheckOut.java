f441f9c86c021e37e20e9a39444d9f57f261f0b2
mgodfreyyoung <mgodfreyyoung@gmail.com>
2024-04-17 18:33:30 -0600

    added in the validateDiscountPercentage which throws an exception if the discount percentage is not in range of 0 to 100.

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -8,6 +8,12 @@ public class CheckOut {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        try {
+            validateDiscountPercentage(discountPercentage);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     // validate that the rental day count is at least one or greater
@@ -15,6 +21,12 @@ public class CheckOut {
     public void validateRentalDayCount(int rentalDayCount) throws Exception {
         if (rentalDayCount < 1) {
             throw new Exception("Rental day count is not 1 or greater. Please enter a value for number of rental days that is at least 1.");
+        }
+    }
+
+    public void validateDiscountPercentage(float discountPercentage) throws Exception {
+        if (discountPercentage < 0 || discountPercentage > 100) {
+            throw new Exception("Discount percentage is not in the range of 0 - 100.  Please enter a value from 0 to 100.");
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        CheckOut checkOut = new CheckOut("CHNS", 0, 0.0f, new Date());
+        CheckOut checkOut = new CheckOut("CHNS", 1, 101.0f, new Date());
     }
 }
