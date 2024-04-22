eb741f69574f3aa3de6ae6c0cabea216b011a95f
mgodfreyyoung <mgodfreyyoung@gmail.com>
2024-04-21 19:09:29 -0600

    added in unit test for the rental agreement output.

--- tests/CheckOutTest.java
+++ tests/CheckOutTest.java
@@ -2,10 +2,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+
+import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.Calendar;
 
 public class CheckOutTest {
+
 
     @Before
     public void initialize() {
@@ -22,24 +26,23 @@ public class CheckOutTest {
             new CheckOut("JAKR", 5, 101, calendar);
         } catch (Exception e) {
             Assert.assertEquals("Error message incorrect for discount", "Discount percentage is not in the range of 0 - 100.  Please enter a value from 0 to 100.", e.getMessage());
-            System.out.println(e.getMessage());
         }
     }
 
     @Test
     public void checkoutTestTwo() {
         try {
-            var checkOutDate = LocalDate.of(2020, 7, 2);
+            final var checkOutDate = LocalDate.of(2020, 7, 2);
 
-            var checkOut = new CheckOut("LADW", 3, 10, checkOutDate);
+            final var checkOut = new CheckOut("LADW", 3, 10, checkOutDate);
 
-            var rentalAgreementTestDecorator =  checkOut.rentalAgreement.new TestDecorator();
+            final var rentalAgreementTestDecorator = checkOut.rentalAgreement.new TestDecorator();
 
             // test expected due date
 
             // since our original calendar was 7/2/20 (mm//dd//yyy) we need to make sure the
             // rental agreement has a due date of 7/5/20 (adding 3 days to checkoutDate)
-            var expectedDueDate = LocalDate.of(2020, 7, 5);
+            final var expectedDueDate = LocalDate.of(2020, 7, 5);
             Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate, rentalAgreementTestDecorator.dueDate());
 
             // test number of chargeable rental days
@@ -52,15 +55,36 @@ public class CheckOutTest {
 
             // check pre-discount charge
             // daily charge of $1.99 x 2 = $3.98
-            Assert.assertEquals("pre-discount amount incorrect", 3.98, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(),0);
+            Assert.assertEquals("pre-discount amount incorrect", 3.98, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(), 0);
 
             // discount amount: 3.98  x 10% = .40 (0.398 rounded up)
-            Assert.assertEquals("discount total incorrect",.40, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
+            Assert.assertEquals("discount total incorrect", .40, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
 
             // final amount: 3.98 - .40 = $3.58
-            Assert.assertEquals("final total incorrect",3.58, rentalAgreementTestDecorator.finalCharge().doubleValue(),0);
+            Assert.assertEquals("final total incorrect", 3.58, rentalAgreementTestDecorator.finalCharge().doubleValue(), 0);
+
+            // redirect the output that would normally go to the console to a print stream
+            // so we can make sure it is correct
+            final var outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
 
             rentalAgreementTestDecorator.printAgreement();
+
+            final var outputStreamString = outputStream.toString();
+            Assert.assertEquals("rental agreement output incorrect.", outputStreamString,
+                    "Tool code: LADW\n" +
+                            "Tool type: Ladder\n" +
+                            "Tool brand: Werner\n" +
+                            "Rental days: 3\n" +
+                            "Check out date: 07/02/20\n" +
+                            "Due date: 07/05/20\n" +
+                            "Daily rental charge: $1.99\n" +
+                            "Charge days: 3\n" +
+                            "Pre-discount charge: $3.98\n" +
+                            "Discount percent: 10%\n" +
+                            "Discount amount: $0.40\n" +
+                            "Final charge: $3.58\n\n");
+
 
         } catch (Exception e) {
 
@@ -71,18 +95,18 @@ public class CheckOutTest {
     @Test
     public void checkoutTestThree() {
         try {
-            var checkOutDate = LocalDate.of(2015, 7, 2);
+            final var checkOutDate = LocalDate.of(2015, 7, 2);
 
-            var checkOut = new CheckOut("CHNS", 5, 25, checkOutDate);
+            final var checkOut = new CheckOut("CHNS", 5, 25, checkOutDate);
 
-            var rentalAgreementTestDecorator =  checkOut.rentalAgreement.new TestDecorator();
+            final var rentalAgreementTestDecorator = checkOut.rentalAgreement.new TestDecorator();
 
             // test expected due date
 
             // since our original calendar was 7/2/20 (mm//dd//yyy) we need to make sure the
             // rental agreement has a due date of 7/7/20 (adding 5 days to checkoutDate)
-            var expectedDueDate = LocalDate.of(2015, 7, 7);
-            Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate,  rentalAgreementTestDecorator.dueDate());
+            final var expectedDueDate = LocalDate.of(2015, 7, 7);
+            Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate, rentalAgreementTestDecorator.dueDate());
 
             // test number of chargeable rental days
             // number of charge days should be 3 - 5 total days rental - 2 (weekend days)
@@ -90,15 +114,35 @@ public class CheckOutTest {
 
             // check pre-discount charge
             // daily charge of $1.49 x 2 = $4.47
-            Assert.assertEquals("pre-discount amount incorrect", 4.47, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(),0);
+            Assert.assertEquals("pre-discount amount incorrect", 4.47, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(), 0);
 
             // discount amount: 4.47  x 25% = 1.12
-            Assert.assertEquals("discount total incorrect",1.12, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
+            Assert.assertEquals("discount total incorrect", 1.12, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
 
             // final amount: 4.47 - 1.12 = 3.35
-            Assert.assertEquals("final total incorrect",3.35, rentalAgreementTestDecorator.finalCharge().doubleValue(),0);
+            Assert.assertEquals("final total incorrect", 3.35, rentalAgreementTestDecorator.finalCharge().doubleValue(), 0);
+
+            // redirect the output that would normally go to the console to a print stream
+            // so we can make sure it is correct
+            final var outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
 
             rentalAgreementTestDecorator.printAgreement();
+
+            final var outputStreamString = outputStream.toString();
+            Assert.assertEquals("rental agreement output incorrect.", outputStreamString,
+                    "Tool code: CHNS\n" +
+                            "Tool type: Chainsaw\n" +
+                            "Tool brand: Stihl\n" +
+                            "Rental days: 5\n" +
+                            "Check out date: 07/02/15\n" +
+                            "Due date: 07/07/15\n" +
+                            "Daily rental charge: $1.49\n" +
+                            "Charge days: 5\n" +
+                            "Pre-discount charge: $4.47\n" +
+                            "Discount percent: 25%\n" +
+                            "Discount amount: $1.12\n" +
+                            "Final charge: $3.35\n\n");
 
         } catch (Exception e) {
 
@@ -109,11 +153,11 @@ public class CheckOutTest {
     @Test
     public void checkoutTestFour() {
         try {
-            var checkOutDate = LocalDate.of(2015, 9, 3);
+            final var checkOutDate = LocalDate.of(2015, 9, 3);
 
-            var checkOut = new CheckOut("JAKD", 6, 0, checkOutDate);
+            final var checkOut = new CheckOut("JAKD", 6, 0, checkOutDate);
 
-            var rentalAgreementTestDecorator =  checkOut.rentalAgreement.new TestDecorator();
+            final var rentalAgreementTestDecorator = checkOut.rentalAgreement.new TestDecorator();
 
             // test expected due date
 
@@ -121,7 +165,7 @@ public class CheckOutTest {
             // jackhammers only have charges on weekdays so 3 days total chargeable.(skipping weekends)
             // Labor day falls on the 7th so no charge that day plus 2 weekend days
             // total chargeable = 3 days chargeable out of the 6 rental days
-            var expectedDueDate = LocalDate.of(2015, 9, 9);
+            final var expectedDueDate = LocalDate.of(2015, 9, 9);
             Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate, rentalAgreementTestDecorator.dueDate());
 
             // test number of chargeable rental days
@@ -132,15 +176,35 @@ public class CheckOutTest {
 
             // check pre-discount charge
             // daily charge of $2.99 x 3 = $8.97
-            Assert.assertEquals("pre-discount amount incorrect", 8.97, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(),0);
+            Assert.assertEquals("pre-discount amount incorrect", 8.97, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(), 0);
 
             // discount amount: 8.97  x 0 = 0
-            Assert.assertEquals("discount total incorrect",0, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
+            Assert.assertEquals("discount total incorrect", 0, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
 
             // final amount: 8.97 - 0 = 8.97
-            Assert.assertEquals("final total incorrect",8.97, rentalAgreementTestDecorator.finalCharge().doubleValue(),0);
+            Assert.assertEquals("final total incorrect", 8.97, rentalAgreementTestDecorator.finalCharge().doubleValue(), 0);
+
+            // redirect the output that would normally go to the console to a print stream
+            // so we can make sure it is correct
+            final var outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
 
             rentalAgreementTestDecorator.printAgreement();
+
+            final var outputStreamString = outputStream.toString();
+            Assert.assertEquals("rental agreement output incorrect.", outputStreamString,
+                    "Tool code: JAKD\n" +
+                            "Tool type: JakeHammer\n" +
+                            "Tool brand: DeWalt\n" +
+                            "Rental days: 6\n" +
+                            "Check out date: 09/03/15\n" +
+                            "Due date: 09/09/15\n" +
+                            "Daily rental charge: $2.99\n" +
+                            "Charge days: 6\n" +
+                            "Pre-discount charge: $8.97\n" +
+                            "Discount percent: 0%\n" +
+                            "Discount amount: $0.00\n" +
+                            "Final charge: $8.97\n\n");
 
         } catch (Exception e) {
 
@@ -151,18 +215,18 @@ public class CheckOutTest {
     @Test
     public void checkoutTestFive() {
         try {
-            var checkOutDate = LocalDate.of(2015, 7, 2);
+            final var checkOutDate = LocalDate.of(2015, 7, 2);
 
-            var checkOut = new CheckOut("JAKR", 9, 0, checkOutDate);
+            final var checkOut = new CheckOut("JAKR", 9, 0, checkOutDate);
 
-            var rentalAgreementTestDecorator =  checkOut.rentalAgreement.new TestDecorator();
+            final var rentalAgreementTestDecorator = checkOut.rentalAgreement.new TestDecorator();
 
             // test expected due date
 
             // Total days = July 3rd to 11th = 9 rental days
             // charge on weekdays only
             // 6 chargeable days (excluding weekends and holidays)
-            var expectedDueDate = LocalDate.of(2015, 7, 11);
+            final var expectedDueDate = LocalDate.of(2015, 7, 11);
             Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate, rentalAgreementTestDecorator.dueDate());
 
             // test number of chargeable rental days
@@ -171,15 +235,36 @@ public class CheckOutTest {
 
             // check pre-discount charge
             // daily charge of $2.99 x 5 = 17.94
-            Assert.assertEquals("pre-discount amount incorrect", 17.94, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(),0);
+            Assert.assertEquals("pre-discount amount incorrect", 17.94, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(), 0);
 
             // discount amount: 17.94  x 0 = 0
-            Assert.assertEquals("discount total incorrect",0, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
+            Assert.assertEquals("discount total incorrect", 0, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
 
             // final amount: 17.94 - 0 = 17.94
-            Assert.assertEquals("final total incorrect",17.94, rentalAgreementTestDecorator.finalCharge().doubleValue(),0);
+            Assert.assertEquals("final total incorrect", 17.94, rentalAgreementTestDecorator.finalCharge().doubleValue(), 0);
+
+            // redirect the output that would normally go to the console to a print stream
+            // so we can make sure it is correct
+            final var outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
 
             rentalAgreementTestDecorator.printAgreement();
+
+            final var outputStreamString = outputStream.toString();
+            Assert.assertEquals("rental agreement output incorrect.", outputStreamString,
+                    "Tool code: JAKR\n" +
+                            "Tool type: JakeHammer\n" +
+                            "Tool brand: Ridgid\n" +
+                            "Rental days: 9\n" +
+                            "Check out date: 07/02/15\n" +
+                            "Due date: 07/11/15\n" +
+                            "Daily rental charge: $2.99\n" +
+                            "Charge days: 9\n" +
+                            "Pre-discount charge: $17.94\n" +
+                            "Discount percent: 0%\n" +
+                            "Discount amount: $0.00\n" +
+                            "Final charge: $17.94\n\n");
+
         } catch (Exception e) {
 
             Assert.fail("an exception was thrown which should not have been; message = " + e.getMessage());
@@ -189,15 +274,15 @@ public class CheckOutTest {
     @Test
     public void checkoutTestSix() {
         try {
-            var checkOutDate = LocalDate.of(2020, 7, 2);
+            final var checkOutDate = LocalDate.of(2020, 7, 2);
 
-            var checkOut = new CheckOut("JAKR", 4, 50, checkOutDate);
+            final var checkOut = new CheckOut("JAKR", 4, 50, checkOutDate);
 
-            var rentalAgreementTestDecorator =  checkOut.rentalAgreement.new TestDecorator();
+            final var rentalAgreementTestDecorator = checkOut.rentalAgreement.new TestDecorator();
 
             // test due date
             // due date = 2 - 6 = July 6th
-            var expectedDueDate = LocalDate.of(2020, 7, 6);
+            final var expectedDueDate = LocalDate.of(2020, 7, 6);
             Assert.assertEquals("rental agreement date does not match our expected date.", expectedDueDate, rentalAgreementTestDecorator.dueDate());
 
             // test number of chargeable rental days
@@ -206,15 +291,37 @@ public class CheckOutTest {
 
             // check pre-discount charge
             // daily charge of $2.99 x 2 = 5.98
-            Assert.assertEquals("pre-discount amount incorrect", 5.98, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(),0);
+            Assert.assertEquals("pre-discount amount incorrect", 5.98, rentalAgreementTestDecorator.preDiscountAmount().doubleValue(), 0);
 
             // discount amount: 5.98  x .50 = 2.99
-            Assert.assertEquals("discount total incorrect",2.99, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
+            Assert.assertEquals("discount total incorrect", 2.99, rentalAgreementTestDecorator.discountAmount().doubleValue(), 0);
 
             // final amount: 5.98 - 2.99 = 2.99
-            Assert.assertEquals("final total incorrect",2.99, rentalAgreementTestDecorator.finalCharge().doubleValue(),0);
+            Assert.assertEquals("final total incorrect", 2.99, rentalAgreementTestDecorator.finalCharge().doubleValue(), 0);
+
+            // redirect the output that would normally go to the console to a print stream
+            // so we can make sure it is correct
+            final var outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
 
             rentalAgreementTestDecorator.printAgreement();
+
+            final var outputStreamString = outputStream.toString();
+            Assert.assertEquals("rental agreement output incorrect.", outputStreamString,
+                    "Tool code: JAKR\n" +
+                            "Tool type: JakeHammer\n" +
+                            "Tool brand: Ridgid\n" +
+                            "Rental days: 4\n" +
+                            "Check out date: 07/02/20\n" +
+                            "Due date: 07/06/20\n" +
+                            "Daily rental charge: $2.99\n" +
+                            "Charge days: 4\n" +
+                            "Pre-discount charge: $5.98\n" +
+                            "Discount percent: 50%\n" +
+                            "Discount amount: $2.99\n" +
+                            "Final charge: $2.99\n\n");
+
+
         } catch (Exception e) {
 
             Assert.fail("an exception was thrown which should not have been; message = " + e.getMessage());
